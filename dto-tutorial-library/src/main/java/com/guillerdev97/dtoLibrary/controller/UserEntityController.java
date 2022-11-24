d0503cc418bab730e9e02bdd50e05ff5960b3334
Guillermo García <guillermieres97@gmail.com>
2022-11-24 17:25:36 +0100

    create user entity controller with one get method

---
+++ dto-tutorial-library/src/main/java/com/guillerdev97/dtoLibrary/controller/UserEntityController.java
@@ -0,0 +1,27 @@
+package com.guillerdev97.dtoLibrary.controller;
+
+import com.guillerdev97.dtoLibrary.dto.UserLocationDTO;
+import com.guillerdev97.dtoLibrary.service.UserEntityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserEntityController {
+    private UserEntityService userEntityService;
+
+    @Autowired
+    public UserEntityController(UserEntityService userEntityService) {
+        this.userEntityService = userEntityService;
+    }
+
+    @GetMapping("/users/location")
+    public ResponseEntity<List<UserLocationDTO>> getAllUsersLocation() {
+        return ResponseEntity.ok().body(userEntityService.getAllUsersLocation());
+    }
+}
