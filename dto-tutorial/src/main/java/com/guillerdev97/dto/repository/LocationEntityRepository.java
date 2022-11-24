579316857cfe3df9409bcda57c51d4679d8bb9cd
Guillermo García <guillermieres97@gmail.com>
2022-11-24 11:29:53 +0100

    create user entity controller with one get method

--- dto-tutorial/src/main/java/com/guillerdev97/dto/repository/LocationEntityRepository.java
+++ dto-tutorial/src/main/java/com/guillerdev97/dto/repository/LocationEntityRepository.java
@@ -2,6 +2,8 @@ package com.guillerdev97.dto.repository;
 
 import com.guillerdev97.dto.model.LocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationEntityRepository extends JpaRepository<LocationEntity, Long> {
 }
--- dto-tutorial/src/main/java/com/guillerdev97/dto/repository/UserEntityRepository.java
+++ dto-tutorial/src/main/java/com/guillerdev97/dto/repository/UserEntityRepository.java
@@ -3,6 +3,8 @@ package com.guillerdev97.dto.repository;
 import com.guillerdev97.dto.model.UserEntity;
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 }
